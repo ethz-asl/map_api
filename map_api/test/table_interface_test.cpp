c7e3238ca407b336fe3072d1205c738ed67170c6
Titus Cieslewski <dev@titus-c.ch>
2014-03-31 13:16:17 +0200

    But wait, there's more: More table interface tests (previous comments not fixed yet)

--- map_api/test/table_interface_test.cpp
+++ map_api/test/table_interface_test.cpp
@@ -36,3 +36,50 @@ TEST(TableInterFace, initEmpty){
   (*structure)["owner"];
   EXPECT_DEATH((*structure)["garbage"],"^");
 }
+
+
+// TODO(tcies) preprocessor magic to cover all field types?
+class StringFieldTestTable : public TestTable{
+ public:
+  virtual bool init(){
+    setup("string_field_test_table");
+    return true;
+  }
+  Hash insert(const std::string &value){
+    std::shared_ptr<TableInsertQuery> query = getTemplate();
+    (*query)["test_field"]->set_stringvalue(value);
+    return insertQuery(*query);
+  }
+  std::string get(const Hash &id){
+    std::shared_ptr<TableInsertQuery> row = getRow(id);
+    return (*row)["test_field"]->stringvalue();
+  }
+ protected:
+  virtual bool define(){
+    addField("test_field", proto::TableFieldDescriptor_Type_STRING);
+    return true;
+  }
+};
+
+TEST(TableInterface, stringFieldInit){
+  StringFieldTestTable table;
+  table.init();
+  std::shared_ptr<TableInsertQuery> structure = table.templateForward();
+  EXPECT_EQ(structure->fieldqueries_size(), 3);
+  // expecting no death:
+  (*structure)["test_field"];
+}
+
+TEST(TableInterface, stringFieldCreateRead){
+  StringFieldTestTable table;
+  table.init();
+  Hash createTest = table.insert("Test");
+  EXPECT_EQ(table.get(createTest), "Test");
+}
+
+TEST(TableInterface, stringFieldUpdateRead){
+  // TODO(tcies)
+}
+
+// TODO(simon) any idea on how to elegantly do the last 3 tests for all
+// kinds of fields (string, double, int, blob etc...
