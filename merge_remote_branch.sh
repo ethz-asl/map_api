659e43e7ab3e3b052c9ec4cfde802e7c1766e982
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-04-30 20:49:25 +0200

    minor

--- merge_remote_branch.sh
+++ merge_remote_branch.sh
@@ -16,7 +16,7 @@ branch_name=${branch_name:-HEAD}
 echo -e "\e[1;31mThis will merge \"ethz-asl_$1 $2\" into your local branch \"$branch_name\"! \e[0m"
 
 while true; do
-    read -p "Continue?" yn
+    read -p "Continue? [y/n]" yn
     case $yn in
         [Yy]* ) git fetch ethz-asl_$1 $2 && git subtree pull --prefix $1 ethz-asl_$1 $2; break;;
         [Nn]* ) exit;;
