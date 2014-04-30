7e80ae3aa319673c97617f9f0ae130e68a0920be
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-04-30 20:48:05 +0200

    added a merge script

---
+++ merge_remote_branch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No argument for the repository supplied. Usage: pull_branch.sh reposity_name branch_name"
+fi
+if [ -z "$2" ]
+  then
+    echo "No argument for the branch supplied. Usage: pull_branch.sh reposity_name branch_name"
+fi
+
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+
+echo -e "\e[1;31mThis will merge \"ethz-asl_$1 $2\" into your local branch \"$branch_name\"! \e[0m"
+
+while true; do
+    read -p "Continue?" yn
+    case $yn in
+        [Yy]* ) git fetch ethz-asl_$1 $2 && git subtree pull --prefix $1 ethz-asl_$1 $2; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+
