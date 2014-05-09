a4e96c41a819b8dc3b31d7ba967adf0b09c129b4
Titus Cieslewski <dev@titus-c.ch>
2014-05-09 10:59:40 +0200

    Indentation, anticipating fixing of thirdparty linking.

--- aslam_map_manager/map_api/cmake/FindPoco.cmake
+++ aslam_map_manager/map_api/cmake/FindPoco.cmake
@@ -11,8 +11,8 @@ find_library(PocoData_LIBRARY PocoData)
 find_library(PocoSQLite_LIBRARY PocoSQLite)
 
 set(POCO_LIBRARIES ${PocoFoundation_LIBRARY}
-                     ${PocoData_LIBRARY}
-                     ${PocoSQLite_LIBRARY})
+                   ${PocoData_LIBRARY}
+                   ${PocoSQLite_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Poco DEFAULT_MSG
