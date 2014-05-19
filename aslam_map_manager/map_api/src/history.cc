592e68926b9b6f980564c953ce9c3e272f7a4e2d
Titus Cieslewski <dev@titus-c.ch>
2014-05-19 13:30:28 +0200

    minor

--- aslam_map_manager/map_api/src/history.cc
+++ aslam_map_manager/map_api/src/history.cc
@@ -14,31 +14,4 @@ void History::define(){
   addField<Time>("time");
 }
 
-// TODO(tcies) remove once not useful any more
-//std::shared_ptr<Revision> History::revisionAt(const Id& id,
-//                                              const Time& time){
-//  typedef std::shared_ptr<Revision> RevisionPtr;
-//  RevisionPtr revisionIterator = rawGetRow(id);
-//  if (!revisionIterator){
-//    LOG(ERROR) << "Couldn't find id " << id << " in history table " << name();
-//    return RevisionPtr();
-//  }
-//  Time revisionTime;
-//  revisionIterator->get("time", &revisionTime);
-//  while (revisionTime > time){
-//    Id previous;
-//    revisionIterator->get("previous", &previous);
-//    revisionIterator = rawGetRow(previous);
-//    if (!revisionIterator){
-//      LOG(ERROR) << "Failed to get previous revision " << previous.hexString();
-//      return RevisionPtr();
-//    }
-//    revisionIterator->get("time", &revisionTime);
-//  }
-//  std::shared_ptr<Revision> returnValue =
-//      std::shared_ptr<Revision>(new Revision);
-//  revisionIterator->get("revision", returnValue.get());
-//  return returnValue;
-//}
-
 } /* namespace map_api */
