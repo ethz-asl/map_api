d54386e13183e9c6578f7cbbee176dbdd54936b6
Titus Cieslewski <dev@titus-c.ch>
2014-05-20 17:05:09 +0200

    Undid hack, should work now...

--- aslam_map_manager/map_api/test/test_main.cpp
+++ aslam_map_manager/map_api/test/test_main.cpp
@@ -6,7 +6,5 @@
 int main(int argc, char **argv){
   google::InitGoogleLogging(argv[0]);
   testing::InitGoogleTest(&argc, argv);
-  std::ofstream db_trunc("database.db", std::ios_base::trunc);
-  db_trunc.close();
   return RUN_ALL_TESTS();
 }
