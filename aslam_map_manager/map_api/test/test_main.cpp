29dafa9f5d225680a26ae27c5fb92f01706bae6f
Titus Cieslewski <dev@titus-c.ch>
2014-05-20 17:00:41 +0200

    Dirty trick to force Jenkins to delete the old (bad) metatable schema.

--- aslam_map_manager/map_api/test/test_main.cpp
+++ aslam_map_manager/map_api/test/test_main.cpp
@@ -1,9 +1,12 @@
+#include <fstream>
+
 #include <gtest/gtest.h>
 #include <glog/logging.h>
 
 int main(int argc, char **argv){
   google::InitGoogleLogging(argv[0]);
   testing::InitGoogleTest(&argc, argv);
-  // testing::FLAGS_gtest_death_test_style = "threadsafe";
+  std::ofstream db_trunc("database.db", std::ios_base::trunc);
+  db_trunc.close();
   return RUN_ALL_TESTS();
 }
