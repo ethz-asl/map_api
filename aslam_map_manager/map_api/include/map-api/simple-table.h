5dca4b351abcad60255dad5b47e2a8706a6f38e6
Titus Cieslewski <dev@titus-c.ch>
2014-05-21 18:34:58 +0200

    Applying SimpleTable on map_api::posegraph

--- aslam_map_manager/map_api/include/map-api/simple-table.h
+++ aslam_map_manager/map_api/include/map-api/simple-table.h
@@ -1,28 +1,35 @@
 #ifndef SIMPLE_TABLE_H_
 #define SIMPLE_TABLE_H_
 
+#include <string>
+
+namespace map_api {
+
 /**
  * Defines a table interface of the desired type containing only one field,
  * of type DataType, with name "data" (but use the constant).
  * REVISION_SET, _GET and _ENUM must of course be defined separately
  */
-template<typename TableType, typename DataType, std::string TableName>
+template<typename TableType, typename DataType>
 class SimpleTable : public TableType {
  public:
-  static const std::string kDataField = "data";
-  virtual const std::string name() const override {
-    return TableName;
-  }
+  static const std::string kDataField;
+  /**
+   * This still needs to be implemented - unfortunately, cpp doesn't support
+   * passing string literals through templates.
+   */
+  virtual const std::string name() const override;
   virtual ~SimpleTable() {}
  protected:
   virtual void define() {
-    TableType::addField<TableType>(kDataField);
+    this->template addField<DataType>(kDataField);
   }
 };
 
-template<typename TableType, typename DataType, std::string TableName>
-const std::string SimpleTable<TableType, DataType, TableName>::kDataField =
+template<typename TableType, typename DataType>
+const std::string SimpleTable<TableType, DataType>::kDataField =
     "data_test";
 
+} // namespace map_api
 
 #endif /* SIMPLE_TABLE_H_ */
