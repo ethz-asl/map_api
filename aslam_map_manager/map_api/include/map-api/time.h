35ff0fffe2ca5cde3cfd326d872203e99c7111e5
Titus Cieslewski <dev@titus-c.ch>
2014-05-21 13:20:25 +0200

    Nanosecond time, using <chrono>, not Poco

--- aslam_map_manager/map_api/include/map-api/time.h
+++ aslam_map_manager/map_api/include/map-api/time.h
@@ -1,26 +1,38 @@
 #ifndef TIME_H_
 #define TIME_H_
 
-#include <Poco/DateTime.h>
+#include <cstdint>
 
 namespace map_api {
 
 /**
  * TODO(tcies) time synchronization
  */
-class Time : public Poco::DateTime{
+class Time {
  public:
   /**
    * To deserialize from database.
    */
-  Time(int64_t epochMicroseconds);
+  Time(int64_t nanoseconds);
   /**
    * Current time
    */
   Time();
+
   int64_t serialize() const;
+
+  inline bool operator <=(const Time& other) const;
+
+  inline bool operator >=(const Time& other) const;
+
+  inline bool operator ==(const Time& other) const;
+
+ private:
+  int64_t value_;
 };
 
 } // namespace map_api
+
+#include "map-api/time-inl.h"
 
 #endif /* TIME_H_ */
