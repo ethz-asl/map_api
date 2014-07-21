8b20922a7189b9cf77a4541a0c28ea69f85b75ef
Titus Cieslewski <dev@titus-c.ch>
2014-07-21 11:43:50 +0200

    Comments of #250

--- aslam_map_manager/map_api/src/chord-index.cc
+++ aslam_map_manager/map_api/src/chord-index.cc
@@ -27,20 +27,18 @@ ChordIndex::Key ChordIndex::hash(const DataType& data) {
   digest_stream << data;
   digest_stream.flush();
   const Poco::DigestEngine::Digest& digest = md5.digest();
-  bool digest_still_uchar_vec =
+  constexpr bool digest_still_uchar_vec =
       std::is_same<
       Poco::DigestEngine::Digest, std::vector<unsigned char> >::value;
-  CHECK(digest_still_uchar_vec) <<
-      "Underlying type of Digest changed since Poco 1.3.6";
+  static_assert(digest_still_uchar_vec,
+      "Underlying type of Digest changed since Poco 1.3.6");
   union KeyUnion {
     Key key;
     unsigned char bytes[sizeof(Key)];
   };
-  CHECK_EQ(sizeof(Key), sizeof(KeyUnion));
+  static_assert(sizeof(Key) == sizeof(KeyUnion), "Bad union size");
   KeyUnion return_value;
-  for (size_t i = 0; i < sizeof(Key); ++i) {
-    return_value.bytes[i] = digest[i];
-  }
+  memcpy(return_value.bytes, &digest[0], sizeof(KeyUnion));
   return return_value.key;
 }
 
@@ -379,7 +377,7 @@ bool ChordIndex::retrieveDataLocally(
     LOG(ERROR) << "Data with given key " << key << " does not exist";
     return false;
   }
-  *value = data_[key];
+  *value = found->second;
   return true;
 }
 
