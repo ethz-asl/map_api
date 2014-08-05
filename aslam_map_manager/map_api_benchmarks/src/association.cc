19a8120eb998edd6327d88d72a4e1be0168c3cc3
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-08-05 07:29:45 +0200

    fix error

--- aslam_map_manager/map_api_benchmarks/src/association.cc
+++ aslam_map_manager/map_api_benchmarks/src/association.cc
@@ -5,15 +5,6 @@
 namespace map_api {
 namespace benchmarks {
 
-const map_api::benchmarks::AssociationId& Association::id() const {
-  return id_;
-}
-
-void Association::setId(const map_api::benchmarks::AssociationId& id) {
-  id_ = id;
-  set_id(id.hexString());
-}
-
 const map_api::benchmarks::DataPointId& Association::dataPointId() const {
   return data_point_id_;
 }
@@ -35,8 +26,8 @@ void Association::setCenterId(const map_api::benchmarks::CenterId& center_id) {
 
 bool Association::parse(const std::string& str) {
   return ParseFromString(str)
-      && data_point_id_.fromHexString(proto::DataPoint::id())
-      && center_id_.fromHexString(proto::Center::id());
+      && center_id_.fromHexString(proto::Association::center_id())
+      && data_point_id_.fromHexString(proto::Association::data_point_id());
 }
 
 }  // namespace benchmarks
