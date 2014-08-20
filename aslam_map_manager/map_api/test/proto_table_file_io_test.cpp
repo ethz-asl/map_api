110a44194188188e3b15f766709f7fd3c4824652
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-08-21 01:36:50 +0200

    fixed_test

--- aslam_map_manager/map_api/test/proto_table_file_io_test.cpp
+++ aslam_map_manager/map_api/test/proto_table_file_io_test.cpp
@@ -18,20 +18,21 @@ TEST_P(NetTableTest, SafeAndRestoreFromFile) {
   Chunk* chunk = table_->newChunk();
   CHECK_NOTNULL(chunk);
   Id chunk_id = chunk->id();
+  Id item_1_id = Id::generate();
+  Id item_2_id = Id::generate();
   {
     Transaction transaction;
     std::shared_ptr<Revision> to_insert_1 = table_->getTemplate();
-    to_insert_1->set(CRTable::kIdField, Id::generate());
+    to_insert_1->set(CRTable::kIdField, item_1_id);
     to_insert_1->set(kFieldName, 42);
     std::shared_ptr<Revision> to_insert_2 = table_->getTemplate();
-    to_insert_2->set(CRTable::kIdField, Id::generate());
+    to_insert_2->set(CRTable::kIdField, item_2_id);
     to_insert_2->set(kFieldName, 21);
     transaction.insert(table_, chunk, to_insert_1);
     transaction.insert(table_, chunk, to_insert_2);
     ASSERT_TRUE(transaction.commit());
     CRTable::RevisionMap retrieved;
     LogicalTime dumptime = LogicalTime::sample();
-    LOG(ERROR) << "Store Dumping at " << dumptime.serialize();
     chunk->dumpItems(dumptime, &retrieved);
     ASSERT_EQ(2, retrieved.size());
     CRTable::RevisionMap::iterator it = retrieved.begin();
@@ -43,7 +44,6 @@ TEST_P(NetTableTest, SafeAndRestoreFromFile) {
     it->second->get(CRTable::kInsertTimeField, &time_2);
     it->second->get(kFieldName, &item_2);
     EXPECT_EQ(time_1.serialize(), time_2.serialize());
-    LOG(ERROR) << "Wrote out time " << time_2.serialize();
     EXPECT_EQ(item_1, 42);
     EXPECT_EQ(item_2, 21);
   }
@@ -59,35 +59,30 @@ TEST_P(NetTableTest, SafeAndRestoreFromFile) {
     EXPECT_TRUE(file_io.StoreTableContents(LogicalTime::sample(), *table_));
   }
 
-  const static std::string kTableName2 = "table_restore";
-  std::unique_ptr<TableDescriptor> descriptor2(new TableDescriptor);
-  descriptor2->setName(kTableName2);
-  descriptor2->addField<int>(kFieldName);
-  NetTableManager::instance().addTable(
-      GetParam() ? CRTable::Type::CRU : CRTable::Type::CR, &descriptor2);
-  NetTable* table_2 = &NetTableManager::instance().getTable(kTableName2);
+  // Reset the state of the database.
+  TearDown();
+  SetUp();
 
   {
-    ProtoTableFileIO file_io(*table_2, test_filename);
-    ASSERT_TRUE(file_io.ReStoreTableContents(table_2));
+    ProtoTableFileIO file_io(*table_, test_filename);
+    ASSERT_TRUE(file_io.ReStoreTableContents(table_));
   }
-  LOG(ERROR) << "table1:" << table_->getStatistics();
-  LOG(ERROR) << "table2:" << table_2->getStatistics();
 
   {
-    chunk = table_2->getChunk(chunk_id);
+    chunk = table_->getChunk(chunk_id);
     ASSERT_FALSE(chunk == nullptr);
     CRTable::RevisionMap retrieved;
     LogicalTime time_1, time_2;
     int item_1, item_2;
     LogicalTime dumptime = LogicalTime::sample();
-    LOG(ERROR) << "Restore Dumping at " << dumptime.serialize();
     chunk->dumpItems(dumptime, &retrieved);
     ASSERT_EQ(2, retrieved.size());
-    CRTable::RevisionMap::iterator it = retrieved.begin();
+    CRTable::RevisionMap::iterator it = retrieved.find(item_1_id);
+    ASSERT_FALSE(it == retrieved.end());
     it->second->get(CRTable::kInsertTimeField, &time_1);
     it->second->get(kFieldName, &item_1);
-    ++it;
+    it = retrieved.find(item_2_id);
+    ASSERT_FALSE(it == retrieved.end());
     it->second->get(CRTable::kInsertTimeField, &time_2);
     it->second->get(kFieldName, &item_2);
     EXPECT_EQ(time_1.serialize(), time_2.serialize());
