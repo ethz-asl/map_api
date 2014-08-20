cbf7e38af13d1ef8948c8d352b1c9f0f3fc8121b
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-08-20 17:03:58 +0200

    Added hashing for logical time

--- aslam_map_manager/map_api/include/map-api/logical-time.h
+++ aslam_map_manager/map_api/include/map-api/logical-time.h
@@ -2,13 +2,24 @@
 #define MAP_API_LOGICAL_TIME_H_
 
 #include <cstdint>
-#include <iostream>
+#include <iostream>  // NOLINT
 #include <mutex>
 
 namespace map_api {
+class LogicalTime;
+}  // namespace map_api
 
+namespace std {
+template <>
+struct hash<map_api::LogicalTime> {
+  inline size_t operator()(const map_api::LogicalTime& time) const;
+};
+}  // namespace std
+
+namespace map_api {
 class LogicalTime {
  public:
+  friend size_t std::hash<LogicalTime>::operator()(const LogicalTime&) const;
   /**
    * Invalid time
    */
@@ -43,17 +54,20 @@ class LogicalTime {
   static std::mutex current_mutex_;
 };
 
-} // namespace map_api
+}  // namespace map_api
 
 namespace std {
-
 inline ostream& operator<<(ostream& out, const map_api::LogicalTime& time) {
   out << "Logical time(" << time.serialize() << ")";
   return out;
 }
 
+inline size_t std::hash<map_api::LogicalTime>::operator()(
+    const map_api::LogicalTime& time) const {
+  return std::hash<uint64_t>()(time.value_);
 }
+}  // namespace std
 
 #include "map-api/logical-time-inl.h"
 
-#endif /* MAP_API_LOGICAL_TIME_H_ */
+#endif  // MAP_API_LOGICAL_TIME_H_
