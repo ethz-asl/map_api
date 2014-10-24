4cd25c9eca5b442537fd419866f8d920a75503ed
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-10-24 11:45:58 +0200

    more fixes

--- map-api/test/map_api_fixture.h
+++ map-api/test/map_api_fixture.h
@@ -1,7 +1,7 @@
 #ifndef MAP_API_MAP_API_FIXTURE_H_
 #define MAP_API_MAP_API_FIXTURE_H_
 
-#include <multiprocess_gtest/multiprocess_fixture.h>
+#include <multiprocess-gtest/multiprocess_fixture.h>
 
 class MapApiFixture : public map_api_test_suite::MultiprocessTest {
  protected:
