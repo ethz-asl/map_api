0205cee5cbeb6c75158aa7711c5cf776158c45b5
Titus Cieslewski <dev@titus-c.ch>
2015-01-18 08:54:57 +0100

    Minor fix.

--- map-api/include/map-api/chunk-transaction.h
+++ map-api/include/map-api/chunk-transaction.h
@@ -72,7 +72,7 @@ class ChunkTransaction {
   // INTERNAL
   void prepareCheck(const LogicalTime& check_time,
                     std::unordered_map<Id, LogicalTime>* chunk_stamp);
-  typedef std::unordered_multimap<const NetTable* const, Id> TableToIdMultiMap;
+  typedef std::unordered_multimap<NetTable*, Id> TableToIdMultiMap;
   void getTrackers(const NetTable::NewChunkTrackerMap& overrides,
                    TableToIdMultiMap* trackers) const;
 
