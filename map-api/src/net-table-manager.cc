f750a4068da66d0cab22402b2672175880f48b01
Marcin <marcin.dymczyk@mavt.ethz.ch>
2015-04-09 15:46:53 +0200

    Addr Simon's comments

--- map-api/src/net-table-manager.cc
+++ map-api/src/net-table-manager.cc
@@ -203,8 +203,7 @@ NetTable* NetTableManager::addTable(
 NetTable& NetTableManager::getTable(const std::string& name) {
   CHECK(Core::instance() != nullptr) << "Map API not initialized!";
   tables_lock_.acquireReadLock();
-  std::unordered_map<std::string, std::unique_ptr<NetTable> >::iterator
-  found = tables_.find(name);
+  TableMap::iterator found = tables_.find(name);
   // TODO(tcies) load table schema from metatable if not active
   CHECK(found != tables_.end()) << "Table not found: " << name;
   tables_lock_.releaseReadLock();
@@ -214,8 +213,7 @@ NetTable& NetTableManager::getTable(const std::string& name) {
 const NetTable& NetTableManager::getTable(const std::string& name) const {
   CHECK(Core::instance() != nullptr) << "Map API not initialized!";
   tables_lock_.acquireReadLock();
-  std::unordered_map<std::string, std::unique_ptr<NetTable> >::const_iterator
-  found = tables_.find(name);
+  TableMap::const_iterator found = tables_.find(name);
   // TODO(tcies) load table schema from metatable if not active
   CHECK(found != tables_.end()) << "Table not found: " << name;
   tables_lock_.releaseReadLock();
