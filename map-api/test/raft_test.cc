75a5d95764c1fafbcfab46e4941e18232ed8b3f6
Anwar Ahmad Quraishi <anwr.ah@gmail.com>
2015-04-21 15:03:33 +0200

    adding new test: manyPeersJoin

--- map-api/test/raft_test.cc
+++ map-api/test/raft_test.cc
@@ -204,7 +204,7 @@ TEST_F(ConsensusFixture, BurstAppendEntries) {
     RaftNode::instance().stop();
     IPC::barrier(DIE, kProcesses);
   }
-}*/
+}
 
 TEST_F(ConsensusFixture, OnePeerJoin) {
   const uint64_t kProcesses = 2;  // Processes in addition to supervisor.
@@ -254,6 +254,82 @@ TEST_F(ConsensusFixture, OnePeerJoin) {
     }
     // Check if committed entries are replicated on atleast half of the peers.
     EXPECT_GT(num_complete_log_peers, peer_list.size() / 2);
+  } else if (getSubprocessId() == LEADER) {
+    IPC::barrier(INIT, kProcesses);
+
+    VLOG(1) << "Peer Id " << RaftNode::instance().self_id() << " : PID " << pid;
+    RaftNode::instance().start();
+    usleep(1000 * kMillisecondsToMicroseconds);
+    appendEntriesBurst(40);
+    usleep(4000 * kMillisecondsToMicroseconds);
+    IPC::push(PeerId::self());
+    IPC::barrier(LEADER_ENTRIES_ADD, kProcesses);
+    IPC::barrier(INIT_JOINING_PEER, kProcesses);
+
+    usleep(8000 * kMillisecondsToMicroseconds);
+    IPC::barrier(DIE, kProcesses);
+    RaftNode::instance().stop();
+
+  } else if (getSubprocessId() == JOINING_PEER) {
+    IPC::barrier(INIT, kProcesses);
+    VLOG(1) << "Peer Id " << RaftNode::instance().self_id() << " : PID " << pid;
+    IPC::barrier(LEADER_ENTRIES_ADD, kProcesses);
+    setJoinRequestPeer(IPC::pop<PeerId>());
+    RaftNode::instance().start();
+    IPC::barrier(INIT_JOINING_PEER, kProcesses);
+    IPC::barrier(DIE, kProcesses);
+    RaftNode::instance().stop();
+  }
+}*/
+
+TEST_F(ConsensusFixture, ManyPeerJoin) {
+  const uint64_t kProcesses = 2;  // Processes in addition to supervisor.
+  enum {
+    SUPERVISOR,
+    LEADER,
+    JOINING_PEER
+  };
+  enum Barriers {
+    INIT,
+    LEADER_ENTRIES_ADD,
+    INIT_JOINING_PEER,
+    DIE
+  };
+  pid_t pid = getpid();
+
+  if (getSubprocessId() == SUPERVISOR) {
+    VLOG(1) << "Supervisor Id: " << RaftNode::instance().self_id() << " : PID "
+            << pid;
+    launchSubprocess(LEADER);
+    launchSubprocess(JOINING_PEER);
+    IPC::barrier(INIT, kProcesses);
+    IPC::barrier(LEADER_ENTRIES_ADD, kProcesses);
+    IPC::barrier(INIT_JOINING_PEER, kProcesses);
+
+    IPC::barrier(DIE, kProcesses);
+
+    /*std::set<PeerId> peer_list;
+    std::vector<proto::QueryStateResponse> states;
+    Hub::instance().getPeers(&peer_list);
+    uint64_t max_commit_index = 0;
+    for (const PeerId& peer : peer_list) {
+      proto::QueryStateResponse state = queryState(peer);
+      states.push_back(state);
+
+      if (state.commit_index() > max_commit_index) {
+        max_commit_index = state.commit_index();
+      }
+      VLOG(1) << peer << ": Commit index = " << state.commit_index();
+    }
+    // Number of peers with more logs than commit index.
+    uint num_complete_log_peers = 0;
+    for (proto::QueryStateResponse state : states) {
+      if (state.last_log_index() >= max_commit_index) {
+        ++num_complete_log_peers;
+      }
+    }
+    // Check if committed entries are replicated on atleast half of the peers.
+    EXPECT_GT(num_complete_log_peers, peer_list.size() / 2);*/
   } else if (getSubprocessId() == LEADER) {
     IPC::barrier(INIT, kProcesses);
 
