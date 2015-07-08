711ea10e9cb0358d4b831f4b9e82ea02032aafeb
Anwar Ahmad Quraishi <anwr.ah@gmail.com>
2015-07-08 13:16:39 +0200

    Set leader id to self when tstarting raft as leader.

--- map-api/include/map-api/raft-chunk-inl.h
+++ map-api/include/map-api/raft-chunk-inl.h
@@ -9,6 +9,7 @@ namespace map_api {
 
 void RaftChunk::setStateFollowerAndStartRaft() {
     raft_node_.state_ = RaftNode::State::FOLLOWER;
+    raft_node_.leader_id_ = PeerId::self();
     VLOG(2) << PeerId::self() << ": Starting Raft node as follower for chunk "
             << id_.printString();
     raft_node_.start();
