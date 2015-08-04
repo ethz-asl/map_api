9153da4017c2f4d0a676a0c754c5b9538f91b1f6
Titus Cieslewski <dev@titus-c.ch>
2015-08-04 10:45:40 +0200

    Re-enabling clearing of the discovery file.

--- map-api/include/map-api/test/testing-entrypoint-inl.h
+++ map-api/include/map-api/test/testing-entrypoint-inl.h
@@ -14,7 +14,10 @@ namespace map_api {
 MapApiTestEntryPoint::~MapApiTestEntryPoint() {}
 
 void MapApiTestEntryPoint::customInit() {}
-
+  if (FLAGS_subprocess_id == 0) {
+    std::ofstream truncator(FileDiscovery::kFileName,
+    std::ofstream::out | std::ofstream::trunc);
+  }
 }  // namespace map_api
 
 #endif  // MAP_API_TESTING_ENTRYPOINT_INL_H_
