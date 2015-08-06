c441fc1baa1b20bd5340562914a5d4c992039b34
Titus Cieslewski <dev@titus-c.ch>
2015-08-06 17:37:45 +0200

    Mission names: Map API integration, cleaned that up a little while at it.

--- map-api/include/map-api/app-templates.h
+++ map-api/include/map-api/app-templates.h
@@ -30,6 +30,42 @@ void objectToRevision(const ObjectType& object, map_api::Revision* revision);
 template <typename TrackeeType, typename TrackerType, typename TrackerIdType>
 TrackerIdType determineTracker(const TrackeeType& trackee);
 
+#define MAP_API_SIMPLE_TYPE_REVISION_CONVERSION(Type, ProtoType)             \
+  template <>                                                                \
+  std::shared_ptr<Type> objectFromRevision<Type>(const Revision& revision) { \
+    std::shared_ptr<Type> result(new Type);                                  \
+    ProtoType proto;                                                         \
+    revision.get(0, &proto);                                                 \
+    result->deserialize(proto);                                              \
+    return result;                                                           \
+  }                                                                          \
+  template <>                                                                \
+  void objectToRevision(const Type& object, Revision* revision) {            \
+    CHECK_NOTNULL(revision);                                                 \
+    ProtoType proto;                                                         \
+    object.serialize(&proto);                                                \
+    revision->set(0, proto);                                                 \
+  }
+
+#define MAP_API_SIMPLE_TYPE_WITH_ID_REVISION_CONVERSION(Type, ProtoType,     \
+                                                        IdType)              \
+  template <>                                                                \
+  std::shared_ptr<Type> objectFromRevision<Type>(const Revision& revision) { \
+    std::shared_ptr<Type> result(new Type);                                  \
+    ProtoType proto;                                                         \
+    revision.get(0, &proto);                                                 \
+    result->deserialize(revision.getId<IdType>(), proto);                    \
+    return result;                                                           \
+  }                                                                          \
+  template <>                                                                \
+  void objectToRevision(const Type& object, Revision* revision) {            \
+    CHECK_NOTNULL(revision);                                                 \
+    ProtoType proto;                                                         \
+    object.serialize(&proto);                                                \
+    revision->set(0, proto);                                                 \
+    revision->setId(object.id());                                            \
+  }
+
 }  // namespace map_api
 
 #endif  // MAP_API_APP_TEMPLATES_H_
